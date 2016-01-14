bbff8c3dff9ad0bb47e8f84733740956a318a4b9
JiaHao-git <takeit_easy@126.com>
2016-01-14 13:34:33 +0800

    还原到原始版本HelloGit，再增加一个显示内存的方法

--- FirstGitDemo/src/com/studygit/HelloGit.java
+++ FirstGitDemo/src/com/studygit/HelloGit.java
@@ -1,9 +1,18 @@
 package com.studygit;
 
+import java.util.Date;
+
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit");
-		System.out.println("第二次提交");
-		System.out.println("ABCDE");
+		//还原到了main方法原始版本：新增：
+		showMemory();
+	}
+	
+	public static void showMemory(){
+		Long freeMem = Runtime.getRuntime().freeMemory();//单位是byte
+		Long maxMem = Runtime.getRuntime().maxMemory();
+		System.out.println("可用内存="+ freeMem/1024 + " KB" );
+		System.out.println("最大内存="+ maxMem/1024 + " KB" );
 	}
 }
