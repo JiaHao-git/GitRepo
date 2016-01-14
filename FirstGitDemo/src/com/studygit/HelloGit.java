d989c0737696df29ef76b5801f580914228a0846
JiaHao-git <takeit_easy@126.com>
2016-01-14 15:33:30 +0800

    增加了显示系统属性的方法

--- FirstGitDemo/src/com/studygit/HelloGit.java
+++ FirstGitDemo/src/com/studygit/HelloGit.java
@@ -1,12 +1,19 @@
 package com.studygit;
 
 import java.util.Date;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit");
 		//还原到了main方法原始版本：新增：
 		showMemory();
+		//新增显示系统属性方法
+		showNativeSystem();
 	}
 	
 	public static void showMemory(){
@@ -14,5 +21,13 @@ public class HelloGit {
 		Long maxMem = Runtime.getRuntime().maxMemory();
 		System.out.println("可用内存="+ freeMem/1024 + " KB" );
 		System.out.println("最大内存="+ maxMem/1024 + " KB" );
+	}
+	public static void showNativeSystem(){
+		Properties p = System.getProperties();
+		Set<Entry<Object, Object>> set = p.entrySet();
+		java.util.Iterator<Entry<Object, Object>> iter = set.iterator();
+		while(iter.hasNext()){
+			System.out.println(iter.next());
+		}
 	}
 }
